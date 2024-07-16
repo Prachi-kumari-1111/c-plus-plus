b63d7734913a2ef8e28103c878923e32bb87ab99
Prachi-kumari-1111 <prachikri8840@gmail.com>
2024-07-16 17:42:58 +0530

    e:\C++\c-plus-plus\sumoftwono.cpp

---
+++ sumoftwono.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter 1st number";
+    cin>>x;
+    int y;
+    cout<<"enter second number";
+    cin>>y;
+    cout<<"sum is :"<<x+y;
+}
