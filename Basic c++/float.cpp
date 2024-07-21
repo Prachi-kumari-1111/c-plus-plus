a63b713941b53079d7bd917f7e75200a39d38455
Prachi-kumari-1111 <prachikri8840@gmail.com>
2024-07-21 10:17:18 +0530

    e:\C++\c-plus-plus\Basic c++

---
+++ Basic c++/float.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x=3.15;
+    float y=6.10;
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+}
---
+++ Basic c++/variables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;//declaration
+    x=5;//initialization
+    int y=3;//both
+    
+    cout<<x+y;
+}
