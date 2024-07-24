666554fc8b3281612439502036a60c402d2fe95f
Prachi-kumari-1111 <prachikri8840@gmail.com>
2024-07-24 09:43:47 +0530

    c:\Users\subin\OneDrive\Documents\GitHub\c-plus-plus\conditionls\divisibleby5or3.cpp

---
+++ conditionls/divisibleby5or3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    if(n%5==0 or n%3==0) {
+    cout<<"divisible by 5 or 3"<<endl;
+    }
+   else
+   {
+    cout<<"not divisible by 5 or 3"<<endl;
+   }
+}
