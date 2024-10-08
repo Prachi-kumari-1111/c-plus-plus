6effad608099025d94db7208c1145254063cbbc1
Prachi-kumari-1111 <prachikri8840@gmail.com>
2024-10-08 23:16:26 +0530

    e:\Lets Learn C++\c-plus-plus\conditionls\greater.cpp

---
+++ conditionls/greater.cpp
@@ -0,0 +1,34 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     int b; 
+//     cin>>a;
+//     cout<<"Enter the value a"<<endl;
+//     cin>>b;
+//     cin>>b;
+//     cout<<"Enter the value b"<<endl;
+//     if(a>b){
+//         cout<<"A is greater"<<endl;
+//     }
+//    if(b>a){
+//         cout<<"B is greater"<<endl;
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+  int a;
+cout<<"Enter the value a"<<endl;
+cin>>a;
+if(a>0){
+  cout<<"A is positive"<<endl;
+}
+  else if (a<0){
+  cout<<"A is negative"<<endl;
+  }
+  else{
+    cout<<"A is 0"<<endl;
+  }
+}
