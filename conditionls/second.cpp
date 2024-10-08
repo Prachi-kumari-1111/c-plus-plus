bdc5ce7fbc41ec76933d7b8831d49777ebcc23da
Prachi-kumari-1111 <prachikri8840@gmail.com>
2024-10-08 23:18:39 +0530

    e:\Lets Learn C++\c-plus-plus\conditionls\second.cpp

---
+++ conditionls/second.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+    using namespace std;
+    int main(){
+        int a =124;
+        cout << a << endl;
+
+        char b='v';
+        cout<< b << endl;
+
+        bool bl=true;
+        cout<< bl<< endl;
+
+        float f=1.63;
+        cout<<f<< endl;
+        double d=1.654;
+        cout<<d<<endl;
+        int size=sizeof (b);
+        cout<<"size of b is:" <<size<<endl;
+
+        int x=54;
+        cout<<x<<endl;
+        char y='r';
+        cout<<y<<endl;
+        float f=1.34;
+        cout<<f<<endl;
+        double m=1.90;
+        cout<<m<<endl;
+     }
