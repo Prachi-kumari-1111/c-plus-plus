5cf53e00e414701d11c80c9fb42ba47150b5cc10
Prachi-kumari-1111 <prachikri8840@gmail.com>
2024-11-27 23:45:21 +0530

    e:\Lets Learn C++\c-plus-plus\first.cpp

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world"<<endl;
+     }
+
+    // #include<iostream>
+    // using namespace std;
+    // int main(){
+    //     int a='124';
+    //     cout<<a<<endl;
+
+    //     char b='v';
+    //     cout<<b<<endl;
+    //  }
