4a49a54ccfa8b20ca77f1cbcee85d9d957bbefcb
Prachi-kumari-1111 <163761454+Prachi-kumari-1111@users.noreply.github.com>
2024-12-10 21:34:07 +0530

    Add files via upload

---
+++ conditionls/prime.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cin>>n;
+    // int i=2;
+    // while (i<n){
+    //     if(n%i==0){
+    //         cout<<"Not prime"<<endl;
+    //         return 0;
+    //     }
+    //     // cout<<"prime"<<endl;
+    //     // return 0;
+    // }
+
+
+    int n;
+    cin>>n;
+    int i=2;
+    while(i<n){
+        if(n%i==0){
+            cout<< "Not prime"<< endl;
+            return 0;
+        }
+        i=i+1;
+    }
+    cout<<"Prime"<<endl;
+    return 0;
+}
